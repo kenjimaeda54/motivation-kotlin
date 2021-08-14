e33b5545a8d7221f8636fc5c72c3b82aacd8fb66
kenjimaeda54 <kenjimaedafamily@gmail.com>
2021-08-14 00:17:17 -0300

    logic img screen main activy

--- app/src/main/java/infra/MotivationConstants.kt
+++ app/src/main/java/infra/MotivationConstants.kt
@@ -1,9 +1,18 @@
 package infra
 
+
+
 class MotivationConstants private constructor(){
 
     object Key {
        const val PERSON_NAME = "name"
+    }
+
+    object  Phrase {
+        const val HAPPY = 1
+        const val ALL  = 2
+        const val SUM = 3
+
     }
 
 }
--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -2,24 +2,69 @@ package ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.e.motivation.R
 import infra.MotivationConstants
 import infra.SecuritySharedPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var sharedPreferences: SecuritySharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonNewPhrase.setOnClickListener(this)
+        imgAll.setOnClickListener(this)
+        imgHappy.setOnClickListener(this)
+        imgSum.setOnClickListener(this)
 
-        if(supportActionBar  != null ){
+        if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
         sharedPreferences = SecuritySharedPreferences(this)
         val name = sharedPreferences.getString(MotivationConstants.Key.PERSON_NAME)
         textName.text = name
 
+
     }
+
+
+    override fun onClick(view: View) {
+        val listImg = listOf(R.id.imgAll, R.id.imgHappy, R.id.imgSum)
+        val id = view.id;
+        if (id == R.id.buttonNewPhrase) {
+            generateNewPhrase()
+        } else if (id in listImg) {
+            filterPhase(id)
+        }
+    }
+
+    private fun filterPhase(id: Int) {
+
+        imgAll.setColorFilter(resources.getColor(R.color.white))
+        imgHappy.setColorFilter(resources.getColor(R.color.white))
+        imgSum.setColorFilter(resources.getColor(R.color.white))
+
+
+        when (id) {
+            R.id.imgAll -> {
+                imgAll.setColorFilter(resources.getColor(R.color.purple_200))
+            }
+            R.id.imgHappy -> {
+                imgHappy.setColorFilter(resources.getColor(R.color.purple_200))
+            }
+            R.id.imgSum -> {
+                imgSum.setColorFilter(resources.getColor(R.color.purple_200))
+            }
+
+
+        }
+
+    }
+    private fun generateNewPhrase(){
+
+    }
+
 }
+
